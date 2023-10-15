f1cc9022268e6a3b96933d6204f9d574ae8d960b
VIKAS-1007 <vikasrana7867@gmail.com>
2023-10-15 08:25:09 +0530

    array methods

---
+++ rev.js
@@ -0,0 +1,16 @@
+let arr1=["one","two","three","four"];
+console.log(arr1);
+
+
+arr1.unshift(111,23,535);
+console.log(arr1);
+
+let marvelHeroes=["Ironman","spiderman","hulk"];
+let dcHeroes=["batman","superman","wonderwoman"];
+let ufc=["gsp","connor","duBronx"];
+
+let a="34";
+console.log(Array.from(a));
+
+
+console.log(Array.of(23,24,null,undefined))
