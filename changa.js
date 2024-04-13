7a625108b8810fec53251e9f57a1e6ed31864b0d
VIKAS-1007 <112776472+VIKAS-1007@users.noreply.github.com>
2024-04-13 11:31:38 +0530

    Create changa.js

---
+++ changa.js
@@ -0,0 +1,64 @@
+// let myRide = new ride("pagani", "0-100 in I am about to find the fuck out");
+
+// function a() {
+//   this.name = "vikas";
+// }
+
+// let b = new a();
+
+// console.log(a.__proto__ === a.prototype);
+// console.log(
+//   a.__proto__.__proto__ === Object.prototype
+// ); /*matlab saarey objects Object function k instance hai*/
+
+// console.log(Function.__proto__ === Function.prototype);
+// console.log(Function.__proto__.__proto__ === Object.prototype);
+// console.log(Object.__proto__ === Object.prototype);
+// console.log(Function.__proto__ === Function.constructor);
+// console.log(Function.__proto__);
+// console.log(Function.prototype);
+// console.log(Function.constructor);
+// console.log(`
+
+//   `);
+
+// console.log(Object.__proto__);
+// console.log(Object.prototype);
+// console.log(Object.constructor);
+
+// console.log(a.__proto__);
+// console.log(a.prototype);
+// console.log(a.constructor);
+
+// console.log(`
+//   `);
+
+// console.log(b.__proto__);
+// console.log(b.prototype);
+// console.log(b.constructor);
+
+// const c = function () {
+//   console.log("jai shree ram");
+// };
+
+// console.log(c.constructor);
+
+// if (
+//   a.constructor === c.constructor &&
+//   c.constructor === Object.constructor &&
+//   c.constructor === Function.constructor
+// ) {
+//   console.log("golmaal");
+// }
+
+// console.log(Object.prototype);
+// console.log(a.constructor);
+// console.log(b.constructor);
+// console.log(Array.constructor);
+// console.log(Object.constructor);
+// console.log(Function.constructor);
+
+// console.log(Object.__proto__);
+// console.log(Array.__proto__);
+// console.log(Function.__proto__);
+// console.log(Object.__proto__.constructor);
